1c2e5378235229681be100c97e6d000750ddde43
Jonathan Agarrado (Desktop) <jet320boy@gmail.com>
2024-01-08 02:50:01 +0800

    Add background image

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Untitled Python Game")
 
+BACKGROUND = pygame.image.load('Windows-XP-BG.png')
+
+def draw():
+    WIN.blit(BACKGROUND, (0, 0))
+    pygame.display.update()
+
+
 def main():
     run = True
 
@@ -14,7 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
-    
+                
+        draw()
+        
     pygame.quit()
 
 if __name__ == "__main__":
