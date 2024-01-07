e2bc082316aaf7a1321dc5bba3313b035e31db8c
Jonathan Agarrado (Desktop) <jet320boy@gmail.com>
2024-01-08 02:42:42 +0800

    Add main function to handle game loop

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import random
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Untitled Python Game")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+    pygame.quit()
+    
