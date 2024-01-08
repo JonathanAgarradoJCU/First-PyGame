563e7ed770e6caff0de96a922c134ad63d0b6dbe
DalanDan <jet320boy@gmail.com>
2024-01-08 15:26:00 +0800

    Update player and star colors, and darken background image

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def draw(player, elapsed_time, stars):
     time_text = FONT.render(f"{round(elapsed_time)}s", 1, (0, 0, 0))
     WIN.blit(time_text, (10, 10))
 
-    pygame.draw.rect(WIN, (255, 0, 0), player)
+    pygame.draw.rect(WIN, (255, 255, 0), player)
 
     for star in stars:
-        pygame.draw.rect(WIN, (0, 0, 255), star)
+        pygame.draw.rect(WIN, (255, 0, 0), star)
 
     pygame.display.update()
 
@@ -57,18 +57,20 @@ def main():
         star_count += clock.tick(60)
         elapsed_time = time.time() - start_time
 
-        if star_count > star_add_increment and len(stars) < 10:  # Limit the number of stars to 10
-            if star_count % 100 == 0:  # Add a delay of 100 ticks between each star spawn
-                for _ in range(3):
-                    star_x = random.randint(0, WIDTH - STAR_WIDTH)  # Provide the lower bound as 0 and the upper bound as WIDTH - STAR_WIDTH
-                    star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
-                    stars.append(star)
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT,
+                                   STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_a] and player.x - PLAYER_VELOCITY >= 0:
             player.x -= PLAYER_VELOCITY
@@ -90,9 +92,17 @@ def main():
                 stars.remove(star)
                 hit = True
                 break
+            
+        if hit:
+            lost_text = FONT.render("LOL YOU SUCK NOOB", 1, (0, 0, 0))
+            lost_text = pygame.transform.scale(lost_text, (270, 90))
+            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(4000)
+            break
 
         draw(player, elapsed_time, stars)
-        
+
     pygame.quit()
 
 
